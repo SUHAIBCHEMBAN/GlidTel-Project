78b90d63df2be6ec2c8e926c4bc44abb160b5866
shuhaib <shuhaibchemban2004@gmail.com>
2024-08-24 20:23:41 +0530

    add first app and details

--- Glidtel/Main/urls.py
+++ Glidtel/Main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('shope',views.shope,name='shope'),
+    path('service',views.service,name='service'),
 ]
--- Glidtel/Main/views.py
+++ Glidtel/Main/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render,redirect
 
 def home(request):
     return render(request,'home.html')
+
+def shope(request):
+    return render(request,'shope.html')
+
+def service(request):
+    return render(request,'service.html')
