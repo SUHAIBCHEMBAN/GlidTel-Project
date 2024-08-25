41328f034ab62a6cf32e1440c6eff6ac9b9e1876
shuhaib <shuhaibchemban2004@gmail.com>
2024-08-25 22:28:15 +0530

    add login

--- Glidtel/Accounts/views.py
+++ Glidtel/Accounts/views.py
@@ -1,10 +1,28 @@
+import random
+from django.core.mail import send_mail
 from django.shortcuts import render,redirect
-
+from django.contrib.auth import get_user_model
 # Create your views here.
 
 def login(request):
     if request.user.is_authenticated:
-        return render(request,'home.html')
+        return redirect(request,'home')
+    
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        request.session['email'] = email
+        username = request.POST.get('username')
+
+        user = get_user_model().objects.filter(email=email).first()
+        if user:
+            otp = ''.join(random.choices('01234589',k=4))
+            request.session['otp'] = otp
+            mail_subject = 'Glide Tel Mobiles'
+            message = f'Your OTP is {otp}, This Code Not Share!'
+            send_mail(mail_subject,message,'your_email@example.com',[email])
+        else:
+            user = get_user_model().objects.create_user(username=username,email=email)
+            return redirect(request,'otp_verify')
     return render(request,'login.html')
 
 def otp_verify(request):
